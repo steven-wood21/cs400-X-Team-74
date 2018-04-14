ae55561f3224709199de4d3c5cc058ad6382a419
Steven Wood <sdwood3@wisc.edu>
2018-04-14 13:31:30 -0500

    Added inner classes

--- Graph.java
+++ Graph.java
@@ -16,6 +16,48 @@ public class Graph<E> implements GraphADT<E> {
     /**
      * Instance variables and constructors
      */
+    public class Vertex<E>{
+        private E element;
+
+        public Vertex(E element){
+            this.element = element;
+        }
+
+        public E getElement(){
+            return element;
+        }
+
+        public void setElement(E element){
+            this.element = element
+        }
+    }
+
+
+    public class Edge{
+        private Vertex source;
+        private Vertex destination;
+
+        public Edge(Vertex source, Vertex destination){
+            this.source = source;
+            this.destination = destination;
+        }
+
+        public Node getDestination() {
+            return destination;
+        }
+
+        public void setDestination(Node destination) {
+            this.destination = destination;
+        }
+        public Node getSource() {
+            return source;
+        }
+
+        public void setSource(Node source) {
+            this.source = source;
+        }
+
+    }
 
     /**
      * {@inheritDoc}

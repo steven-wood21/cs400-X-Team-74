5556d7ae515de1cbb25dd79a9d79f216c7624e48
adwinter1 <37051533+adwinter1@users.noreply.github.com>
2018-04-14 16:05:36 -0500

    Working Files Upload

--- GraphProcessor.java
+++ GraphProcessor.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**

7511823d39d10aa42f0531e94f631b77b604f1ed
Zac Wille <zwille@wisc.edu>
2018-04-15 17:04:08 -0500

    commented graph

--- Graph.java
+++ Graph.java
@@ -109,21 +109,47 @@ public class Graph<E> implements GraphADT<E> {
             this.destination = destination;
         }
         
+        /**
+         * Getter used to receive the one of the vertices in the edge.
+         * 
+         * @return destination - one of the vertices of the edge
+         */
         public Vertex getDestination() {
             return destination;
         }
 
+        /**
+         * Setter used to set one of the vertices in the edge
+         * 
+         * @param destination - one of the vertices of the edge
+         */
         public void setDestination(Vertex destination) {
             this.destination = destination;
         }
+        
+        /**
+         * Getter used to receive the other vertex in the edge.
+         * 
+         * @return source - the other vertex of the edge
+         */
         public Vertex getSource() {
             return source;
         }
-
+        
+        /**
+         * Setter used to set the other vertex in the edge
+         * 
+         * @param source - the other vertex of the edge
+         */
         public void setSource(Vertex source) {
             this.source = source;
         }
         
+        /**
+         * Contains method used to test if a vertex is part of an edge
+         * 
+         * @param key - a vertex to be tested if part of an edge
+         */
         public boolean contains(E key) {
         	if (key.equals(source.getElement()) || 
         			key.equals(destination.getElement())) {
@@ -139,7 +165,8 @@ public class Graph<E> implements GraphADT<E> {
     @Override
     public E addVertex(E vertex) {
     	if ((vertex != null) && !(vertices.containsKey(vertex))) {
-        	vertices.put(vertex, new Vertex<>(vertex));
+        	vertices.put(vertex, new Vertex<>(vertex)); 
+        		//adding a new vertex to the HashMap
         	return vertex;
         } return null;
     }
@@ -152,7 +179,11 @@ public class Graph<E> implements GraphADT<E> {
     	if ((vertex != null) && (vertices.containsKey(vertex))) {
     		Iterator<Edge> i = neighbors.iterator();
     		
-    		while (i.hasNext()) {
+    		/* Iterating through each edge connected to the vertex 
+			 * given in the parameter to delete all edges connected
+			 * to the vertex being deleted. 
+			 */
+    		while (i.hasNext()) { 
     			Edge e = i.next();
     			if (e.contains(vertex)) {
     				i.remove();
@@ -170,7 +201,9 @@ public class Graph<E> implements GraphADT<E> {
     public boolean addEdge(E vertex1, E vertex2) {
     	if ((vertices.containsKey(vertex1) && vertices.containsKey(vertex2)) 
     			&& !(vertex1.equals(vertex2))) {
+    		
     		neighbors.add(new Edge(vertices.get(vertex1), vertices.get(vertex2)));
+    		
     		return true;
     	} return false;
     }    
@@ -182,11 +215,18 @@ public class Graph<E> implements GraphADT<E> {
     public boolean removeEdge(E vertex1, E vertex2) {
     	if ((vertices.containsKey(vertex1) && vertices.containsKey(vertex2)) 
     			&& !(vertex1.equals(vertex2))) {
+    		
     		for (int i = 0; i < neighbors.size(); i++) {
+    			
+    			/* Looping through each element in the neighbors array to find the 
+    			 * correct edge to delete. 
+    			 */
+    		
     			if ((neighbors.get(i).getDestination().element.equals(vertex1) && 
     					neighbors.get(i).getSource().element.equals(vertex2)) || 
     					(neighbors.get(i).getSource().element.equals(vertex1) && 
     					neighbors.get(i).getDestination().element.equals(vertex2))) {
+    				
     				neighbors.remove(i);
     			}
     		}
@@ -201,10 +241,16 @@ public class Graph<E> implements GraphADT<E> {
     public boolean isAdjacent(E vertex1, E vertex2) {
     	if ((vertices.containsKey(vertex1) && vertices.containsKey(vertex2)) 
     			&& !(vertex1.equals(vertex2))) {
+    		
+    		/* Looping through each vertex that is a neighbor of vertex1 in order 
+    		 * to test if vertex2 is adjacent to vertex1 or not. 
+    		 */
 	        for (E i : getNeighbors(vertex1)) {
+	        	
 	        	if (i.equals(vertex2)) {
 	        		return true;
 	        	}
+	        	
 	        } return false;
     	} return false;
     }
@@ -217,10 +263,16 @@ public class Graph<E> implements GraphADT<E> {
         if (!(vertex == null) && (vertices.containsKey(vertex))) {
         	ArrayList<E> localNeighbors = new ArrayList<E>();
         	
+        	/* Looping through each edge in the neighbors array list
+        	 * in order to find which vertices are the neighbors of 
+        	 * the vertex given in the parameter. 
+        	 */
         	for (Edge i : neighbors) {
+        		
         		if (i.getDestination().getElement().equals(vertex)) {
         			localNeighbors.add((E) i.getSource().getElement());
-        		} else if (i.getSource().getElement().equals(vertex)) {
+        		} 
+        		else if (i.getSource().getElement().equals(vertex)) {
         			localNeighbors.add((E) i.getDestination().getElement());
         		}
         	}
